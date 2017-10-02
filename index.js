e2baa1372d3783c4fb9c11e138d583ea02161b33
Nathan Campbell <nathancampbell@outlook.com>
2017-10-02 17:26:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-template").innerHTML)
+  Handlebars.registerHelper('displayIngredient', function(ingredient){
+    return new Handlebars.SafeString(ingredient)
+  })
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  var result = template();
+  document.getElementsByTagName("main")[0].innerHTML += result;
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
