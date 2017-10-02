2c993e72fb13a0c7a7ee5161a28ad4450a4837e3
Nathan Campbell <nathancampbell@outlook.com>
2017-10-02 17:26:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+  var name = document.getElementById('name').value;
+  var description = document.getElementById('recipeDescription').value;
+  var ingredientsNodes = document.getElementsByName("ingredients")
+  var ingredients = []
+  for(var i=0;i<ingredientsNodes.length;i++) {
+    ingredients.push(ingredientsNodes[i].value)
+  }
+  var recipe = {name, description, ingredients}
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  var recipeHTML = template(recipe);
+  document.getElementById('main').innerHTML = recipeHTML;
+}
